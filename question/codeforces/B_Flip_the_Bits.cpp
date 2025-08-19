decaa9f5203a5eaf835727779dc1f758e1feae34
Vaidik Saxena <83622368+10vulture1005@users.noreply.github.com>
2025-08-19 14:41:57 +0530

    solved: question/codeforces/B_Flip_the_Bits.cpp

--- question/codeforces/B_Flip_the_Bits.cpp
+++ question/codeforces/B_Flip_the_Bits.cpp
@@ -27,6 +27,112 @@ int n;
 cin>>n;
 string a,b;
 cin>>a>>b;
+vector<int> pa(n);// for 0s
+vector<int> pbs(n);
+for(int i = 0; i < n; i++) {
+    if(i==0){
+        pa[0]=a[i]=='1'?0:1;
+        pbs[0]=b[i]=='1'?0:1;
+        continue;
+    }
+    if(a[i]=='0'){
+        pa[i]=pa[i-1]+1;
+    }else{
+                pa[i]=pa[i-1];
+
+    }
+    if(b[i]=='0'){
+        pbs[i]=pbs[i-1]+1;
+    }else{
+                pbs[i]=pbs[i-1];
+
+    }
+}
+vector<int> idx;
+
+
+for(int i = 0; i < n; i++) {
+    if(pa[i]==pbs[i] and pa[i]==i+1-pa[i]){
+        idx.pb(i);
+    }
+}
+
+
+bool f = 0;
+            if(pa[0]>pbs[0]){
+                f=1;
+            }else{
+                f=0;
+            }
+        
+int j = 0;
+// for(int i = 0; i <n;i++){
+// cout<<pa[i]<<" "<<pbs[i]<<endl;
+// }
+// cout<<endl;
+
+// vout(idx,idx.size());cout<<endl;
+if(idx.size()!=0){
+for(int i = 1; i <= idx[idx.size()-1]; i++) {
+if(j>=idx.size())break;
+    if(i==idx[j]){
+        if(idx[j++]<n-1){
+            if(pa[i+1]>pbs[i+1]){
+                f=1;
+            }else if(pa[i+1]<pbs[i+1]){
+                f=0;
+            }else{
+                continue;
+            }
+            continue;
+        }
+    }
+
+
+
+
+
+
+
+
+    if(f){
+        if(((pa[i]+pbs[i]==i+1) and pa[i]>=pbs[i]) or ((pa[i]-pbs[i]==0) and pa[i]==pbs[i])){
+            continue;
+        }else{
+            no 
+            return;
+        }
+    }else{
+        if(((pa[i]+pbs[i]==i+1) and pa[i]<=pbs[i]) or (((pa[i]-pbs[i]==0) and pa[i]==pbs[i]))){
+            continue;
+        }else{
+            no 
+
+            return;
+        }
+    }
+}
+
+
+for(int i = idx[idx.size()-1]+1; i < n; i++) {
+    if(pa[i]!=pbs[i]){
+        no
+        return;
+    }
+}
+yes
+}else{
+for(int i = 0; i < n; i++) {
+    if(pa[i]!=pbs[i]){
+        no 
+        return;
+    }
+}
+yes
+}
+
+
+
 
 }
 signed main(){
