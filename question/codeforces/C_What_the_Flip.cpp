1cc18314f171ec291f636f25646c49a0bf08e001
Vaidik Saxena <83622368+10vulture1005@users.noreply.github.com>
2025-08-24 15:34:28 +0530

    solved: question/codeforces/B_Quantbulbs_Fused.cpp question/codeforces/C_What_the_Flip.cpp question/codeforces/D_Find_the_Different_Ones.cpp question/codeforces/F_Yet_Another_Problem_About_Pairs_Satisfying_an_Inequality.cpp question/codeforces/iicpc.cpp

---
+++ question/codeforces/C_What_the_Flip.cpp
@@ -0,0 +1,50 @@
+//b87678
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+#define int long long
+#define gcd(a, b) (__gcd(a, b))
+#define vin(a,n) for(int i=0;i<n;++i) cin>>a[i];
+#define vout(a,n) for(int i=0;i<n;++i) cout<<a[i]<<' ';
+#define all(a) (a.begin(), a.end()); 
+#define pb push_back
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+#define vi vector<int>
+#define input_tej_le ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace  std;
+/*
+ ██████     ██████     ██████  
+██         ██     ██   ██    ██ 
+██   ███   ██     ██   ██    ██ 
+██    ██   ██     ██   ██    ██ 
+ ██████     ██████     ██████  
+==========================================================
+Author: Vaidik Saxena
+From : IIITL
+==========================================================
+*/
+const long long MOD = 1e9 + 7;
+
+void vulture(){
+int n;
+cin>>n;
+// fixing num of s
+int ans = 0;
+for(int i = 0; i <=n; i++) {
+    int ts = n-i;
+    ts = n-2*i+1;
+    
+
+    ans+=()%MOD;
+
+}
+}
+signed main(){
+input_tej_le;
+int t;
+cin>>t;
+while(t--){
+vulture();
+}
+}
---
+++ question/codeforces/iicpc.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+#define int long long
+#define gcd(a, b) (__gcd(a, b))
+#define vin(a,n) for(int i=0;i<n;++i) cin>>a[i];
+#define vout(a,n) for(int i=0;i<n;++i) cout<<a[i]<<' ';
+#define all(a) (a.begin(), a.end()); 
+#define pb push_back
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+#define vi vector<int>
+#define input_tej_le ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace  std;
+/*
+ ██████     ██████     ██████  
+██         ██     ██   ██    ██ 
+██   ███   ██     ██   ██    ██ 
+██    ██   ██     ██   ██    ██ 
+ ██████     ██████     ██████  
+==========================================================
+Author: Vaidik Saxena
+From : IIITL
+==========================================================
+*/
+
+
+const long long MOD = 1e9 + 7;
+long long modpow(long long a, long long b) {
+    long long res = 1;
+    a=a%MOD;
+    while (b > 0) {
+        if (b & 1) res = (res * a) %MOD;
+        a = (a * a) %MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+long long modinv(long long q) {
+    return modpow(q, MOD - 2);
+}
+void vulture(){
+cout<<modinv(2);
+
+}
+signed main(){
+input_tej_le;
+int t;
+cin>>t;
+while(t--){
+vulture();
+}
+}
