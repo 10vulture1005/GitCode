ec7dcee2b10fd6ce690a19472adc74b5dccfc8e4
Vaidik Saxena <83622368+10vulture1005@users.noreply.github.com>
2025-08-25 16:45:20 +0530

    solved: question/codeforces/D_Find_the_Different_Ones.cpp

--- question/codeforces/D_Find_the_Different_Ones.cpp
+++ question/codeforces/D_Find_the_Different_Ones.cpp
@@ -27,13 +27,40 @@ int n;
 cin>>n;
 vector<int> a(n);
 vin(a,a.size());
+vector<int> idx(n,-1);
+int pr = 0;
+int idxs = 0;
+for(int i = 0; i < n; i++) {
+    if(a[pr]!=a[i]){
+        idx[pr]=i+1;
+        pr = i;
+        idxs=pr+1;
+    }
+}
+for(int i = 1; i < pr; i++) {
+    if(idx[i]==-1){
+        idx[i]=idx[i-1];
+    }
+}
+// for(int i = 0; i <n;i++){
+// cout<<idx[i]<<" ";
+// }
+// cout<<endl;
+
+
 int q;
 cin>>q;
 while(q--){
-    int a,b;
-    cin>>a>>b;
-    
+    int l,r;
+    cin>>l>>r;
+    if(idx[l-1]<=r and idx[l-1]!=-1){
+        cout<<l<<' '<<idx[l-1]<<endl;
+    }else{
+        cout<<"-1 -1"<<endl;
+    }
+      
 }
+cout<<endl;
 }
 signed main(){
 input_tej_le;
