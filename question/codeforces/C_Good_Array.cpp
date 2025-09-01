4beba03d53f81e6fb702815ae31e166b948cce7d
Vaidik Saxena <83622368+10vulture1005@users.noreply.github.com>
2025-09-01 18:23:34 +0530

    solved: question/codeforces/C_Good_Array.cpp

--- question/codeforces/C_Good_Array.cpp
+++ question/codeforces/C_Good_Array.cpp
@@ -25,13 +25,55 @@ From : IIITL
 void vulture(){
 int n;
 cin>>n;
-vector<int> a(n);
+vector<pair<int,int>> a(n);
+for(int i = 0; i < n; i++) {
+    cin>>a[i].first;
+    a[i].second=i+1;
+}
+vector<int> b(n);
+if(n==2){
+    cout<<0<<endl;return;
+}
 
+vector<int> ans;
+sort all(a);
+for(int i = 0; i < n; i++) {
+    b[i] = a[i].first;
+}int sum = accumulate(b.begin(),b.end(),0LL);
+
+for(int i = 0; i < n; i++) {
+    sum-=b[i];
+    if(sum%2==0){
+        
+        int ts = sum/2;
+        auto it = lower_bound(b.begin(),b.end(),ts);
+        if(it!=b.end() and *it==ts){        
+
+            if(it-b.begin()!=i){
+            ans.pb(a[i].second);
+       }
+            else if(it-b.begin()<n-1 and b[it-b.begin()+1]==*it){
+                            ans.pb(a[i].second);
+
+            }
+        }
+    }
+    sum+=b[i];
+
+}
+if(ans.size()==0){
+    cout<<0<<endl;return;
+}
+cout<<ans.size()<<endl;
+for(int i = 0; i <ans.size();i++){
+cout<<ans[i]<<" ";
+}
+cout<<endl;
 }
 signed main(){
 input_tej_le;
-int t;
-cin>>t;
+int t=1;
+// cin>>t;
 while(t--){
 vulture();
 }
