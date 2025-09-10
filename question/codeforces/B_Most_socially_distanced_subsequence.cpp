55d492c83ca4b3c6acf918156094ab54281e4b5a
10vulture1005 <vaidiksaxena14@gmail.com>
2025-09-11 01:34:59 +0530

    code;

--- question/codeforces/B_Most_socially_distanced_subsequence.cpp
+++ question/codeforces/B_Most_socially_distanced_subsequence.cpp
@@ -59,7 +59,29 @@ From : IIITL
 ==========================================================
 */
 void vulture(){
+int n;
+cin>>n;
+vector<int> a(n+1);
+vin(a,a.size()-1);
+bool o = a[0]<a[1];// 1 for inc and 0 for dec
+ vector<int> ans;
+    ans.push_back(a[0]);  
 
+    for (int i = 1; i < n - 1; i++) {
+        if ((a[i] > a[i - 1] && a[i] > a[i + 1]) ||
+            (a[i] < a[i - 1] && a[i] < a[i + 1])) {
+            ans.push_back(a[i]);
+        }
+    }
+
+    
+
+ans.pb(a[n-1]);
+cout<<ans.size()<<endl;
+for(int i = 0; i <ans.size();i++){
+cout<<ans[i]<<" ";
+}
+cout<<endl;
 }
 signed main(){
 input_tej_le;
